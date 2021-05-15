221e8251e4afb613ccc8c2ed5d11dbce4e78b774
haknkayaa <mail@hakankaya.kim>
2021-05-15 19:04:00 +0300

    update

--- xBee-Receiver/src/main.cpp
+++ xBee-Receiver/src/main.cpp
@@ -1,9 +1,40 @@
 #include <Arduino.h>
 
+//Constants
+//#define LED_BUILTIN 13; //Led to Arduino pin 3 (PWM)
+
+//Variables
+String data;
+
 void setup() {
-// write your initialization code here
+
+    //Start the serial communication
+    Serial1.begin(9600); //Baud rate must be the same as is on xBee module
+    Serial.begin(9600);
+
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-// write your code here
+
+    while (Serial1.available() > 0) {
+        //Read the incoming byte
+         data = Serial1.readString();
+         data.trim();
+
+         Serial.println(data);
+
+        if(data == "LED AC") {
+            digitalWrite(LED_BUILTIN, HIGH);
+            Serial.println("LED Açildi");
+        } else if (data == "LED KAPA") {
+            digitalWrite(LED_BUILTIN, LOW);
+            Serial.println("LED Kapandi");
+        }
+    }
 }
+
+
+
+
+
